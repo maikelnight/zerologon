ce9c40a48bf8a7ac4052ea67807bea0e14720872
Maik Linnemann <maik.linnemann@googlemail.com>
2020-10-16 16:57:44 +0200

    Update checkzerologin_.ps1

--- checkzerologin_.ps1
+++ checkzerologin_.ps1
@@ -4,7 +4,7 @@ $patchReleaseDate = (Get-Date "10.08.2020")
 
 function getEvents($dc){
 
-Get-WinEvent -Logname System -ComputerName $dc | Where-Object {$_.TimeCreated -ge $patchReleaseDate} | Where-Object {$_.Id -eq 5827 -or $_.Id -eq 5828 -or $_.Id -eq 5829 -or $_.Id -eq 5830 -or $_.Id -eq 5831} | Select-Object -Property TimeCreated, Source, EventID, Message
+Get-WinEvent -Logname System -ComputerName $dc | Where-Object {$_.TimeCreated -ge $patchReleaseDate} | Where-Object {$_.Id -eq 5827 -or $_.Id -eq 5828 -or $_.Id -eq 5829 -or $_.Id -eq 5830 -or $_.Id -eq 5831} | Select-Object -Property TimeCreated, Id, Message
 
 }
 
