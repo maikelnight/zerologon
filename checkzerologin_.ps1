5e12440b9f8d9480165f7b22edd1dae5ac12e540
Maik Linnemann <maik.linnemann@googlemail.com>
2020-10-16 16:16:02 +0200

    Create checkzerologin_.ps1

---
+++ checkzerologin_.ps1
@@ -0,0 +1,15 @@
+$dcnames = @("DC1","DC2","DC3")
+
+$patchReleaseDate = (Get-Date "10.08.2020")
+
+function getEvents($dc){
+
+Get-WinEvent -Logname System -ComputerName $dc | Where-Object {$_.TimeCreated -ge $patchReleaseDate} | Where-Object {$_.EventID -eq 5827 -or $_.EventID -eq 5828 -or $_.EventID -eq 5829 -or $_.EventID -eq 5830 -or $_.EventID -eq 5831} | Select-Object -Property TimeCreated, Source, EventID, Message
+
+}
+
+foreach ($dc in $dcnames){
+
+getEvents($dc)
+
+}
